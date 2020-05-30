46a774d9f9c81c090a8158a180bba8a3f8ac4277
PatrickNausha <patrick.nausha@gmail.com>
2020-05-30 14:01:09 -0700

    Add temple pieces.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
-import { Scene, PerspectiveCamera, WebGLRenderer, BoxGeometry, MeshStandardMaterial, Mesh, PointLight } from "three";
+import {
+	Scene,
+	PerspectiveCamera,
+	WebGLRenderer,
+	BoxGeometry,
+	MeshStandardMaterial,
+	MeshBasicMaterial,
+	Mesh,
+	PointLight,
+} from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 const renderer = new WebGLRenderer();
@@ -7,12 +16,23 @@ document.body.appendChild(renderer.domElement);
 
 const scene = new Scene();
 const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 5;
-const controls = new OrbitControls(camera, renderer.domElement);
+camera.position.z = 15;
+new OrbitControls(camera, renderer.domElement);
 
-const geometry = new BoxGeometry();
 const material = new MeshStandardMaterial();
-const cube = new Mesh(geometry, material);
+const bottom = new Mesh(new BoxGeometry(8, 2, 5), material);
+scene.add(bottom);
+const middle = new Mesh(new BoxGeometry(7, 1, 3), material);
+middle.position.set(0.5, 1.5, 0);
+scene.add(middle);
+const front = new Mesh(new BoxGeometry(1, 4, 3), material);
+front.position.set(4.5, 1, 0);
+scene.add(front);
+
+const black = new MeshBasicMaterial({ color: 0 });
+const entry = new Mesh(new BoxGeometry(0.1, 3, 0), black);
+entry.position.set(5, 0.5, 0);
+scene.add(entry);
 
 const lights = [new PointLight(0xffffff, 1, 0), new PointLight(0xffffff, 1, 0), new PointLight(0xffffff, 0.5, 0)];
 
@@ -23,7 +43,6 @@ lights[2].position.set(-100, -200, -100);
 scene.add(lights[0]);
 scene.add(lights[1]);
 scene.add(lights[2]);
-scene.add(cube);
 
 function animate() {
 	requestAnimationFrame(animate);
