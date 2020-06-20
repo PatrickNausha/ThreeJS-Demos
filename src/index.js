20791d33c912918d74fe18cb099ed9bf646dc758
PatrickNausha <patrick.nausha@gmail.com>
2020-06-20 11:51:56 -0700

    Make shadows more awesome.

--- src/index.js
+++ src/index.js
@@ -9,14 +9,23 @@ import {
 	SphereGeometry,
 	AmbientLight,
 	PlaneBufferGeometry,
+	VSMShadowMap,
+	PCFShadowMap,
+	PCFSoftShadowMap,
 } from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { updateStats, toggleStats } from "./debug-stats";
 import { toggleDebugLights, addLight } from "./lights";
 import { slamItOnTheGround } from "./positioning";
 
+const shadowMaps = [PCFSoftShadowMap, VSMShadowMap, PCFShadowMap];
+
+const ambientLightColor = 0x111111;
+
 const renderer = new WebGLRenderer({ antialias: true });
 renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = shadowMaps[0];
+renderer.setClearColor(ambientLightColor);
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
@@ -51,22 +60,27 @@ for (let x = -10; x <= 10; x += 5) {
 	}
 }
 
-// Box for fun
-const box = new Mesh(new BoxGeometry(1, 1, 1), plainMaterial);
-box.position.set(4, 0, 4);
-box.rotateY(45);
-scene.add(box);
-
 // Lights
 const whiteLight = new PointLight(0xffffff, 1, 10, 2);
 whiteLight.castShadow = true;
+whiteLight.shadow.camera.near = 0.001;
+whiteLight.shadow.camera.far = 10;
+whiteLight.shadow.mapSize.width = 512;
+whiteLight.shadow.mapSize.height = 512;
+whiteLight.shadow.radius = 4;
 const redLight = new PointLight(0xff0000, 1, 10, 2);
+redLight.castShadow = true;
+redLight.shadow.camera.near = 0.001;
+redLight.shadow.camera.far = 10;
+redLight.shadow.mapSize.width = 512;
+redLight.shadow.mapSize.height = 512;
+redLight.shadow.radius = 4;
 redLight.castShadow = true;
 
 const sceneLights = [addLight(scene, whiteLight), addLight(scene, redLight)];
 sceneLights[0].group.position.set(3, 4, 3);
 
-var light = new AmbientLight(0x111111); // soft white light
+var light = new AmbientLight(ambientLightColor);
 scene.add(light);
 
 // Main loop
@@ -84,12 +98,16 @@ function step() {
 		3,
 		2 * Math.cos(new Date().getTime() / 1000)
 	);
-	sceneLights[1].group.position.set(1, 4 + 5 * Math.sin(new Date().getTime() / 1000), 3);
+	sceneLights[1].group.position.set(1, 3 + 2 * Math.sin(new Date().getTime() / 1000), 3);
 }
 
+let shadowMapIndex = 0;
 window.document.addEventListener("keydown", (event) => {
 	if (event.isComposing || event.keyCode === "1".charCodeAt(0)) {
 		toggleDebugLights();
 		toggleStats();
+	} else if (event.isComposing || event.keyCode === "2".charCodeAt(0)) {
+		shadowMapIndex++;
+		renderer.shadowMap.type = shadowMaps[shadowMapIndex % shadowMaps.length];
 	}
 });
