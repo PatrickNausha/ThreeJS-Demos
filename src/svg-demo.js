d420052350fe7e496870bb286efdf41336a0e450
PatrickNausha <patrick.nausha@gmail.com>
2020-07-01 19:17:53 -0700

    Clean up some things.

--- src/svg-demo.js
+++ src/svg-demo.js
@@ -6,8 +6,10 @@ import {
 	HemisphereLight,
 	Mesh,
 	Group,
-	DoubleSide,
-	ExtrudeBufferGeometry,
+	MeshBasicMaterial,
+	ShapeBufferGeometry,
+	ExtrudeGeometry,
+	BackSide,
 } from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { SVGLoader } from "three/examples/jsm/loaders/SVGLoader";
@@ -22,41 +24,49 @@ document.body.appendChild(renderer.domElement);
 const scene = new Scene();
 
 // Camera
-const camera = new PerspectiveCamera(28, window.innerWidth / window.innerHeight, 1, 1000);
-camera.position.z = -500;
+const camera = new PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 1000);
+camera.position.z = -160;
 new OrbitControls(camera, renderer.domElement);
 
-const plainMaterial = new MeshStandardMaterial();
-
 const loader = new SVGLoader();
-
 loader.load(
 	"./assets/shine-like-stars-outlined-3.svg",
 	function ({ paths }) {
 		const group = new Group();
 		for (const path of paths) {
-			const material = new MeshStandardMaterial({
+			const extrudeMaterial = new MeshStandardMaterial({
 				color: path.color,
+			});
+
+			const frontGlowMaterial = new MeshBasicMaterial({
+				color: path.color,
+				side: BackSide, // Not sure why it comes out backward by default, but it does.
+				depthTest: false, // Hack: Since we're rendering the ShapeBufferGeometry directly on top of the ExtrudeGeometry, always render on top.
 			});
 
 			const shapes = path.toShapes(true);
 
-			const { fill, stroke } = path.userData.style;
+			const { fill } = path.userData.style;
 			if (fill && fill !== "none") {
+				console.log(shapes.length);
 				for (const shape of shapes) {
-					const geometry = new ExtrudeBufferGeometry(shape, {
-						depth: 2,
+					const extrudeGeometry = new ExtrudeGeometry(shape, {
+						depth: 4,
 						bevelEnabled: false,
 					});
-					const mesh = new Mesh(geometry, material);
+					const extrudeMesh = new Mesh(extrudeGeometry, extrudeMaterial);
+					group.add(extrudeMesh);
+
+					const geometry = new ShapeBufferGeometry(shape);
+					const mesh = new Mesh(geometry, frontGlowMaterial);
 					group.add(mesh);
 				}
 			}
 		}
 
-		group.rotateZ(Math.PI);
+		group.rotateZ(Math.PI); // Not sure why SVG comes out flipped around.
 		group.position.x = 200;
-		group.position.y = 150;
+		group.position.y = 160;
 		scene.add(group);
 	},
 	function (xhr) {
