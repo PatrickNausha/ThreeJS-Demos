7a3c4cb66e3a805f4390fb9e58798efda888d5aa
PatrickNausha <patrick.nausha@gmail.com>
2020-07-03 14:40:33 -0700

    Add confetti boxes.

--- src/particles-text-glow-demo.js
+++ src/particles-text-glow-demo.js
@@ -7,6 +7,9 @@ import {
 	MeshNormalMaterial,
 	FontLoader,
 	TextGeometry,
+	BoxGeometry,
+	Box3,
+	Vector3,
 } from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import helvetikerRegularJson from "three/examples/fonts/helvetiker_regular.typeface.json"; // TODO: Add font license if published to web. https://github.com/mrdoob/three.js/blob/master/examples/fonts/LICENSE
@@ -22,7 +25,7 @@ document.body.appendChild(renderer.domElement);
 const scene = new Scene();
 
 // Camera
-const camera = new PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 1000);
+const camera = new PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 10000);
 camera.position.z = 160;
 new OrbitControls(camera, renderer.domElement);
 
@@ -34,25 +37,83 @@ const fontGeometry = new TextGeometry("GLOW", {
 	curveSegments: 12,
 	bevelEnabled: false,
 });
-const fontMesh = new Mesh(
-	fontGeometry,
-	new MeshNormalMaterial({
-		color: 0xffffff,
-	})
-);
+const fontMaterial = new MeshNormalMaterial();
+const fontMesh = new Mesh(fontGeometry, fontMaterial);
 makeCentered(fontMesh);
 
 const fontGroup = new Group();
 fontGroup.add(fontMesh);
 scene.add(fontGroup);
 
+const confettiBox = new Box3(new Vector3(-1000, -1000, -1000), new Vector3(1000, 1000, 100)); // Box within which to render confetti
+const confetti = [];
+const confettiPieceCount = 100;
+const confettiPieceSize = 20;
+for (let pieceCount = 0; pieceCount < confettiPieceCount; pieceCount++) {
+	const boxGroup = new Group();
+	const material = new MeshNormalMaterial({
+		transparent: true,
+	});
+	const box = new Mesh(new BoxGeometry(confettiPieceSize, confettiPieceSize, confettiPieceSize), material);
+	boxGroup.add(box);
+
+	const orientation = getConfettiStartOrientation();
+	boxGroup.position.copy(orientation.position);
+	boxGroup.rotation.x += orientation.rotationX;
+	boxGroup.rotation.y += orientation.rotationX;
+
+	scene.add(boxGroup);
+	confetti.push({ group: boxGroup, material });
+}
+
+function getConfettiStartOrientation() {
+	const confettiBoxSize = confettiBox.getSize(new Vector3());
+	return {
+		position: new Vector3(
+			Math.floor(Math.random() * confettiBoxSize.x) + confettiBox.min.x,
+			Math.floor(Math.random() * confettiBoxSize.y) + confettiBox.min.y,
+			Math.floor(Math.random() * confettiBoxSize.z) + confettiBox.min.z
+		),
+		rotationX: Math.random(),
+		rotationY: Math.random(),
+	};
+}
+
+function step(duration) {
+	for (const piece of confetti) {
+		piece.group.position.y += duration * 100;
+		piece.group.rotation.x += duration;
+		piece.group.rotation.y += duration;
+		if (piece.group.position.y > confettiBox.max.y) {
+			const orientation = getConfettiStartOrientation();
+			piece.group.position.copy(orientation.position);
+			piece.group.rotation.x += orientation.rotationX;
+			piece.group.rotation.y += orientation.rotationX;
+		}
+
+		// Make more opaque when pointed at us
+		const rotatedVector = new Vector3(0, 0, 1);
+		rotatedVector.applyEuler(piece.group.rotation);
+		piece.material.opacity = new Vector3(0, 0, 1).dot(rotatedVector);
+	}
+}
+
 // Show stats
 toggleStats();
 
 // Main loop
-function animate() {
+let lastTimeStamp;
+function animate(timeStamp) {
 	requestAnimationFrame(animate);
+
+	if (!lastTimeStamp) {
+		lastTimeStamp = timeStamp;
+	}
+	const duration = timeStamp - lastTimeStamp;
+	step(duration / 1000);
+
 	renderer.render(scene, camera);
+	lastTimeStamp = timeStamp;
 	updateStats();
 }
-animate();
+requestAnimationFrame(animate);
